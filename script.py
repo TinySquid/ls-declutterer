6838d274df1da561bf916a4de8b8bd1567956efb
Michael Nunes <tinysquid.git@gmail.com>
2021-06-10 12:27:39 -0700

    Add cli print

--- script.py
+++ script.py
@@ -1,6 +1,41 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.getenv("ACCESS_TOKEN"))
+
+def print_usage():
+    print("usage: {0} [--dry-run] [--resume] [--undo]".format(sys.argv[0]))
+    exit()
+
+
+def missing_vars():
+    print("Missing required .env vars. See README.md for instructions.")
+    exit()
+
+
+def prerun_warning():
+    print(
+        "It is recommended to first run this script with the '--dry-run' flag "
+        "so you can inspect the changes that will be made."
+    )
+    print("Continue anyways? (y/n)")
+
+    choice = str(sys.stdin.read(1))
+
+    if choice == "n":
+        exit()
+
+
+accessToken = os.getenv("ACCESS_TOKEN", None)
+repoPrefix = os.getenv("REPO_PREFIX", None)
+
+if accessToken is None or repoPrefix is None:
+    missing_vars()
+
+# Prompt on full run
+if len(sys.argv) == 1:
+    prerun_warning()
+
+print_usage()
